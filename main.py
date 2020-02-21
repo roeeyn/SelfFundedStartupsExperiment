d08756ec7e9162b71bdeb70c50dc1e7fdbfb7d42
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-02-21 02:06:13 -0600

    Filtrated for http

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ def normalize_twitter_name(twitter_url):
         return re.split(r'twitter.com/', twitter_url)[-1].lower()
     if math.isnan(twitter_url):
         return None
-    raise Exception('valio vrg')
+    raise Exception('not valid values for Twitter Handler')
 
 
 # %% Work with the csv
 # data["Company name"] = data["Company name"].str.strip()
-data["Company Twitter"].apply(normalize_twitter_name)
-
+# data["Company Twitter"] = data["Company Twitter"].apply(normalize_twitter_name)
+# data["Nb. of Employees"] = data["Nb. of Employees"].astype('category')
+# data["What do they do (Verbatim - 10 words max.)   "]
+data[data["Link to website"].str[:8] != "https://"]["Link to website"].head()
 # %% Experiments
 import re
 
